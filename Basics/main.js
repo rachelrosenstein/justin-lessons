e391fb6aeffb7033127c0ff2336584013ba2d390
Rachel Rosenstein <rachelrosenstein1@gmail.com>
2020-09-16 18:51:59 -0400

    This is the beginning of some lessons on JS

---
+++ Basics/main.js
@@ -0,0 +1,24 @@
+/*
+String
+Array
+Boolean
+Integer
+Object
+*/
+var name = "Justin"
+console.log(name.toUpperCase())
+
+var name_array = name.split("")
+
+/* for(Where do I start; when do I stop; how fast do i get there) {
+    what to do until I get there
+}
+*/
+
+console.log(name_array[0])
+console.log(name_array[1])
+
+
+for (var i = 0; i < name_array.length; i++) {
+    console.log(name_array[i])
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+# Justin Lessons
+
+Basics - Week 1- 5
+- JS Exercises until we have mastery
